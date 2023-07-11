83fb555e2d790d816568e95814a0cc94bdeba738
Mark Williams <mark.williams@enable.com>
2023-07-11 17:33:47 +0100

    Improve sub script

--- sub-player.sql
+++ sub-player.sql
@@ -6,7 +6,9 @@ DECLARE @SessionId INT
 SELECT @SessionId = [Id] FROM [Sessions] WHERE [Date] = @SessionDate
 
 UPDATE PlayerSessions SET Status = 2 WHERE SessionId = @SessionId AND PlayerId = @DroppingOutPlayerInitials
-UPDATE PlayerSessions SET Status = 3 WHERE SessionId = @SessionId AND PlayerId = @SubbingInPlayerInitials
 
-INSERT INTO PlayerSessions (PlayerId, SessionId, Status)
-VALUES ('GW', (SELECT TOP 1 Id FROM Sessions ORDER BY Date DESC), 3)
+IF EXISTS (SELECT * FROM PlayerSessions WHERE PlayerId = @SubbingInPlayerInitials AND SessionId = @SessionId)
+	UPDATE PlayerSessions SET Status = 3 WHERE SessionId = @SessionId AND PlayerId = @SubbingInPlayerInitials
+ELSE
+	INSERT INTO PlayerSessions (PlayerId, SessionId, Status)
+	VALUES (@SubbingInPlayerInitials, (SELECT TOP 1 Id FROM Sessions ORDER BY Date DESC), 3)
